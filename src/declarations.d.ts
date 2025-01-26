1f9a76ae5bedd8823b339a5f6246508c21eb4d40
Juyeong Maing <mjy9088@naver.com>
2025-01-26 15:23:20 +0900

    copypaste from https://webgpu.github.io/webgpu-samples/?sample=helloTriangle#red.frag.wgsl

---
+++ src/declarations.d.ts
@@ -0,0 +1,3 @@
+/// <reference types="@webgpu/types" />
+
+declare function readFileSync(path: string): string;
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,74 @@
-alert("Hello world!");
+const redFragWGSL = readFileSync("assets/red.frag.wgsl");
+const triangleVertWGSL = readFileSync("assets/triangle.vert.wgsl");
+import { quitIfWebGPUNotAvailable } from "./util";
+
+const canvas = document.createElement("canvas");
+window.addEventListener("load", () => {
+  document.body.insertBefore(canvas, null);
+});
+const device = await (async () => {
+  const adapter = await navigator.gpu?.requestAdapter();
+  const device = (await adapter?.requestDevice()) ?? null;
+  quitIfWebGPUNotAvailable(adapter, device);
+  return device;
+})();
+
+const context = canvas.getContext("webgpu") as GPUCanvasContext;
+
+const devicePixelRatio = window.devicePixelRatio;
+canvas.width = canvas.clientWidth * devicePixelRatio;
+canvas.height = canvas.clientHeight * devicePixelRatio;
+const presentationFormat = navigator.gpu.getPreferredCanvasFormat();
+
+context.configure({
+  device,
+  format: presentationFormat,
+});
+
+const pipeline = device.createRenderPipeline({
+  layout: "auto",
+  vertex: {
+    module: device.createShaderModule({
+      code: triangleVertWGSL,
+    }),
+  },
+  fragment: {
+    module: device.createShaderModule({
+      code: redFragWGSL,
+    }),
+    targets: [
+      {
+        format: presentationFormat,
+      },
+    ],
+  },
+  primitive: {
+    topology: "triangle-list",
+  },
+});
+
+function frame() {
+  const commandEncoder = device.createCommandEncoder();
+  const textureView = context.getCurrentTexture().createView();
+
+  const renderPassDescriptor: GPURenderPassDescriptor = {
+    colorAttachments: [
+      {
+        view: textureView,
+        clearValue: [0, 0, 0, 0], // Clear to transparent
+        loadOp: "clear",
+        storeOp: "store",
+      },
+    ],
+  };
+
+  const passEncoder = commandEncoder.beginRenderPass(renderPassDescriptor);
+  passEncoder.setPipeline(pipeline);
+  passEncoder.draw(3);
+  passEncoder.end();
+
+  device.queue.submit([commandEncoder.finish()]);
+  requestAnimationFrame(frame);
+}
+
+requestAnimationFrame(frame);
